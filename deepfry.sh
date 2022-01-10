48601cc9196053ea54d1cae653249d9a7fb8ce8d
Cory Arcangel <coryarcangel@users.noreply.github.com>
2022-01-10 12:15:18 +0100

    Resize rand 80-100

--- deepfry.sh
+++ deepfry.sh
@@ -25,20 +25,20 @@ cp "$file" "${destination}/${filename}-step-0.${extension}"    # rename file by 
 for (( i = 1; i < $iterations; i++ )); do
 operation=$((0 + $RANDOM % 5))
   
-# modulate x factor
+# Modulate x factor
 if [[ $operation = "0" ]]; then
     factor=$((100 + $RANDOM % 200))
     # convert $destination/$filename-step-$(($i-1)).jpg -modulate 100,$factor $destination/$filename-step-$i.jpg
     command='convert '$destination'/'$filename'-step-'$(($i-1))'.jpg -modulate 100,'$factor' '$destination'/'$filename'-step-'$i'.jpg'
   
-# compress x quality
+# Compress x quality
 elif [[ $operation = "1" ]];  
 then
     factor=$((1 + $RANDOM % 50))
     # convert $destination/$filename-step-$(($i-1)).jpg -compress JPEG2000 -quality $factor $destination/$filename-step-$i.jpg
     command='convert '$destination'/'$filename'-step-'$(($i-1))'.jpg -compress JPEG2000 -quality '$factor' '$destination'/'$filename'-step-'$i'.jpg'
   
-# brightness x 0xfactor  
+# Brightness x 0xfactor  
 elif [[ $operation = "2" ]];
 then
     factor=$((10 + $RANDOM % 90))
@@ -55,11 +55,11 @@ then
 # Resize 
 elif [[ $operation = "4" ]];
 then
-    	factor=$((1 + $RANDOM % 20))
+    	factor=$((1 + $RANDOM % 20 + 80))
     	# convert $destination/$filename-step-$(($i-1)).jpg -brightness-contrast 0x$factor $destination/$filename-step-$i.jpg
     	command='convert '$destination'/'$filename'-step-'$(($i-1))'.jpg -scale '$factor'  -scale '1200x1200\!' '$destination'/'$filename'-step-'$i'.jpg'
 
-# sharpen x 0xfactor  
+# Sharpen x 0xfactor  
 else
 		factor=$((3 + $RANDOM % 10))
     	# convert $destination/$filename-step-$(($i-1)).jpg -sharpen 0x$factor $destination/$filename-step-$i.jpg
