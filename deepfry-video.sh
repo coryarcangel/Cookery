8ddfc1621a5e46d722421362399f88733a53bcbc
henry <henry@dusendusen.com>
2024-02-05 14:21:31 -0500

    simplified and tightened up bash code in deepfry.sh and deepfry-video.sh

--- deepfry-video.sh
+++ deepfry-video.sh
@@ -11,26 +11,18 @@ frameCheck=0
 # End Defaults
 
 #receive command line parameters
-while [ ! $# -eq 0 ]
-do
-  case "$1" in
-    -f) file=$2
-            ;;
-    -o) destination=$2
-            ;;
-    -g) regenerate=$2
-            ;;
-    -fr) rate=$2
-            ;;
-    -r) replay=$2
-            ;;
-    -d) director=$2
-            ;;
-    -fc) frameCheck=1
-            ;;
-    *) file=$1
-  esac
-  shift
+while [ ! $# -eq 0 ]; do
+    case "$1" in
+        -f) file=$2;;
+        -o) destination=$2;;
+        -g) regenerate=$2;;
+        -fr) rate=$2;;
+        -r) replay=$2;;
+        -d) director=$2;;
+        -fc) frameCheck=1;;
+        *) file=$1
+    esac
+    shift
 done
 
 mkdir -p $destination
@@ -42,55 +34,40 @@ replayname="${replay%.*}"
 seconds=$(ffmpeg -i $file 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }')
 total_frames=$((seconds * rate))  
 
-# if fc is set, echo a message and exit
-
-if [ $frameCheck -eq 1 ]
-then
-        echo "total frames: "$total_frames
-        exit 0
+if [ $frameCheck -eq 1 ]; then
+    echo "total frames: "$total_frames
+    exit 0
 fi
 
 # convert source video into separate frames
 ffmpeg -i $file -r $rate $destination/$filename%05d.jpg
 
-
+# create a subfolder for the cooked frames
 mkdir -p $destination/frames
 
-# if $director is not equal to ""
-if [ -z "$director" ]
-then
-      echo "\$director flag is not given, so use the history file"
-      for i in $(seq -f "%05g" 1 $total_frames); do
+# if $director is not equal to "", use the history file
+if [ -z "$director" ]; then
+    for i in $(seq -f "%05g" 1 $total_frames); do
         echo "frame "$i" / "$total_frames
         ./deepfry.sh -r $replay -o "." -l  $destination/$filename$i.jpg
-      done
+    done
 else
-        echo "director flag is given, so use \$director"
-        $framesRendered=1
-        for i in $(cat "$director/director.txt"); do
-                chunkHistory="$director/history/"$(echo $i | cut -d',' -f1)
-                chunkFramesCheck="$director/history/"$(echo $i | cut -d',' -f2)
-                chunkFrames=$(echo $i | cut -d',' -f2)
-                # if the last line doesnt include a frame
-                if [ -z "$chunkFrames" ] || [ $chunkFramesCheck == $chunkHistory ]
-                then
-                        echo "run $chunkHistory for the rest of the frames" 
-                        for j in $(seq -f "%05g" $framesRendered $total_frames); do
-                                echo "frame "$j" / "$total_frames
-                                echo 'running: ./deepfry.sh -r '$chunkHistory' -o "." -l '$destination'/'$filename''$j'.jpg'
-                                ./deepfry.sh -r $chunkHistory -o "." -l $destination/$filename$j.jpg
-                        done
-                        framesRendered=$(($total_frames+1))
-                else   
-                    echo "run $chunkHistory until frame $chunkFrames"   
-                        for j in $(seq -f "%05g" $framesRendered $chunkFrames); do
-                                echo "frame "$j" / "$total_frames
-                                echo 'running: ./deepfry.sh -r '$chunkHistory' -o "." -l '$destination'/'$filename''$j'.jpg'
-                                ./deepfry.sh -r $chunkHistory -o "." -l $destination/$filename$j.jpg
-                        done
-                        framesRendered=$(($chunkFrames+1))
-                fi
+    $framesRendered=1
+    for i in $(cat "$director/director.txt"); do
+        chunkHistory="$director/history/"$(echo $i | cut -d',' -f1) #history file
+        chunkFrames=$(echo $i | cut -d',' -f2) 
+        # if the last line doesnt include a frame, make it the total number of frames
+        if [ -z "$chunkFrames" ] || [ $chunkHistory == "$director/history/"$(echo $i | cut -d',' -f2) ]; then
+            chunkFrames=$total_frames
+        fi
+        echo "run $chunkHistory until frame $chunkFrames"   
+        for j in $(seq -f "%05g" $framesRendered $chunkFrames); do
+            echo "frame "$j" / "$total_frames
+            echo 'running: ./deepfry.sh -r '$chunkHistory' -o "." -l '$destination'/'$filename''$j'.jpg'
+            ./deepfry.sh -r $chunkHistory -o "." -l $destination/$filename$j.jpg
         done
+        framesRendered=$(($chunkFrames+1))
+    done
 fi
 
 # get rid of any old fried frames in case they're in the way
@@ -105,9 +82,7 @@ cd $destination/frames
 ffmpeg -y -framerate 30 -pattern_type glob -i "*.jpg" -c:v libx264 -pix_fmt yuv420p out.mp4
 
 # clean up
-cd ..
-rm *.jpg
-cp frames/out.mp4 $filename-$replayname-result.mp4
+cd ..; rm *.jpg; cp frames/out.mp4 $filename-$replayname-result.mp4;
 
 # If there is audio in the source file, this line might help
 #ffmpeg -i ../../$file -y -framerate $rate -pattern_type glob -i "$filename*-fried.jpg" -c copy -map 0:1 -map 1:0 -c:v libx264 -pix_fmt yuv420p out.mp4
--- deepfry.sh
+++ deepfry.sh
@@ -13,156 +13,99 @@ replay=''
 while [ ! $# -eq 0 ]
 do
   case "$1" in
-    -f) file=$2
-            ;;
-    -n) iterations=$2
-            ;;
-    -o) destination=$2
-            ;;
-    -s) save=$2
-            ;;
-    -h) history_out=$2
-            ;;
-    -r) replay=$2
-            ;;
-    -l) last=1
-            ;;
+    -f) file=$2;;
+    -n) iterations=$2;;
+    -o) destination=$2;;
+    -s) save=$2;;
+    -h) history_out=$2;;
+    -r) replay=$2;;
+    -l) last=1;;
     *) file=$1
   esac
   shift
 done
 
-if [[ $file =~ ".jpg" || $file =~ ".png" || $file =~ ".jpeg" || $file =~ ".tif" ]]; then
-    echo "$file exists."
-    else
-    echo "$file doesnt exist."
-    exit 0
-fi
+if ! [[ $file =~ ".jpg" || $file =~ ".png" || $file =~ ".jpeg" || $file =~ ".tif" ]]; then exit 0; fi
 
 size=$(identify -format "%[fx:w]x%[fx:h]" $file)
-extension="${file##*.}"
 filename="${file%.*}"
 
-cp "$file" "wrkn.jpg"    # rename file by moving it
+cp "$file" "temp.jpg"
 
 echo 'deep frying file: '$file 'destination: '$destination 'iterations: '$iterations 'size: '$size 'save: '$save
 
-#
-# START TRANSFORM FUNCTIONS
-#
+### START COOK FUNCTIONS
 modulate()  {
     factor=${1:-$((100 + $RANDOM % 200))}
-    command='convert wrkn.jpg -modulate 100,'$factor' wrkn.jpg'
-    echo $command
-    echo 'modulate '$factor >> $history_out
-    $command
+    convert temp.jpg -modulate 100,$factor temp.jpg
+    echo 'modulate '$factor | tee -a $history_out
 }
 compress() {
     factor_compress=${1:-$((1 + $RANDOM % 100))}
     factor_size=${2:-$((25 + $RANDOM % 75))}
-    echo 'compress vars'$factor_compress' '$factor_size
-    command='convert wrkn.jpg -scale '$factor_size'  wrkn.jpg'
-    echo $command
-    $command
-    command='convert wrkn.jpg -compress JPEG2000 -quality '$factor_compress' wrkn.jpg'
-    echo $command
-    $command
-    command='convert wrkn.jpg -scale '$size\!' wrkn.jpg '
-    echo $command
-    $command
-    echo 'compress '$factor_compress' '$factor_size >> $history_out
+    convert temp.jpg -scale $factor_size  temp.jpg; 
+    convert temp.jpg -compress JPEG2000 -quality $factor_compress temp.jpg;
+    convert temp.jpg -scale $size\! temp.jpg 
+    echo 'compress '$factor_compress' '$factor_size | tee -a $history_out # echoes the command to $history_out and stdout
 }
 contrast() {
     factor=${1:-$((10 + $RANDOM % 40))}
-    command='convert wrkn.jpg -brightness-contrast 0x'$factor' wrkn.jpg'
-    echo $command
-    # echo $command >> $history_out
-    echo 'contrast '$factor >> $history_out
-    $command
+    convert temp.jpg -brightness-contrast 0x$factor temp.jpg
+    echo 'contrast '$factor | tee -a $history_out
 }
 resize() {
     factor=${1:-$((1 + $RANDOM % 20 + 80))}
-    command='convert wrkn.jpg -scale '$factor'  -scale '$size\!' wrkn.jpg'
-    echo $command
-    # echo $command >> $history_out
-    echo 'resize '$factor >> $history_out
-    $command
+    convert temp.jpg -scale $factor  -scale $size\! temp.jpg
+    echo 'resize '$factor | tee -a $history_out
 }
 edge(){
     factor=${1:-$((1 + $RANDOM % 30))}
-    command='convert wrkn.jpg -edge '$factor' wrkn.jpg'
-    echo $command
-    # echo $command >> $history_out
-    echo 'edge '$factor >> $history_out
-    $command
+    convert temp.jpg -edge $factor temp.jpg
+    echo 'edge '$factor | tee -a $history_out
 }
 normalize(){
-    command='convert wrkn.jpg -normalize wrkn.jpg'
-    echo $command
-    # echo $command >> $history_out
-    echo 'normalize' >> $history_out
-    $command
+    convert temp.jpg -normalize temp.jpg
+    echo 'normalize' | tee -a $history_out
 }
-sharpen(){
-    factor=${1:-$((3 + $RANDOM % 10))}
-    command='wrkn.jpg -sharpen 0x'$factor' wrkn.jpg'
-    echo $command
-    # echo $command >> $history_out
-    echo 'sharpen '$factor >> $history_out
-    $command
-}
-#
-# END TRANSFORM FUNCTIONS
-#
+### END TRANSFORM FUNCTIONS
 
-if [[ ! -z $replay ]] ; 
-then
+if [[ ! -z $replay ]]; then
     input=$replay
     history_out=/dev/null
     i=1
-    while IFS= read -r line
-    do
+    while IFS= read -r line; do
     echo "$i: "$destination"/"$filename-fried.jpg
     $line
-    if [[  $(($i % $save)) -eq 0  && $last -eq 0  ]];  #dump every so often. 
-    then
-        cp "wrkn.jpg" "$destination/$filename-step-$i.jpg"
+    if [[  $(($i % $save)) -eq 0  && $last -eq 0  ]]; then #save at the $save interval
+        cp "temp.jpg" "$destination/$filename-step-$i.jpg"
     fi
     i=$((i+1))
     done < "$input"
 else   
     rm $history_out
     for (( i = 1; i <= $iterations; i++ )); do
-        #Where are we
-        echo -n $i'-'
-
-        # Random option 
-        operation=$((0 + $RANDOM % 6))
+        printf "%02d-" $i
+        operation=$((0 + $RANDOM % 6)) # Pick a random cook function
         if [[ $operation = "0" ]];  then modulate
-            elif [[ $operation = "1" ]];   then compress
+            elif [[ $operation = "1" ]]; then compress
             elif [[ $operation = "2" ]]; then contrast
             elif [[ $operation = "3" ]]; then resize
             elif [[ $operation = "4" ]]; then edge
             elif [[ $operation = "5" ]]; then normalize
-            else sharpen
         fi
         if [[  $last -eq 0 ]]; then
-            if [  $(($i % $save)) -eq 0 ]  #dump every so often. 
-            then
-                cp "wrkn.jpg" "$destination/$filename-step-$i.jpg"
+            if [  $(($i % $save)) -eq 0 ]; then  #dump every so often. 
+                cp "temp.jpg" "$destination/$filename-step-$i.jpg"
             elif [  $i -eq $iterations]; then
-                cp "wrkn.jpg" "$destination/$filename-step-$i.jpg"
+                cp "temp.jpg" "$destination/$filename-step-$i.jpg"
             fi
         fi
     done
-    #save history to scratch file
     echo "$(date '+%H:%m:%S, %a %b %d, %Y.')" >> scratch.txt
-    cat $history_out >> scratch.txt
+    cat $history_out >> scratch.txt #save history to scratch file
 fi
 
-if [[  $last -eq 1 ]]; then
-    cp "wrkn.jpg" "$destination/$filename-fried.jpg"
-fi
+if [[  $last -eq 1 ]]; then cp "temp.jpg" "$destination/$filename-fried.jpg"; fi
 
 #delete wrnk file
-rm wrkn.jpg
+rm temp.jpg
